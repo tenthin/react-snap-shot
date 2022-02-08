8dea7deb95236011deca41e42c9503f3276a5cf2
Tenzin <tenthinten@gmail.com>
2022-02-08 21:16:06 +0530

    add multiple paths

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Service from './Service';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+      <Route path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/service" component={Service}/>
+      <Route path="/contact" component={Contact}/>
+    </Switch>
+   
+      <h1>Welcome</h1>
+    </>
   );
 }
 
---
+++ src/Service.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Serivce =() => {
+    return (
+        <>
+            <h1>Serivce</h1>
+        </> 
+    )
+
+}
+
+export default Serivce;
