5b236c8647891104b30ab42aaaae840181cbc72b
Tenzin <tenthinten@gmail.com>
2022-02-09 18:29:30 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Contact from './Contact';
 import About from './About';
 import Service from './Service';
-
+import Navbar from './Navbar';
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Switch>
-      <Route path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/service" component={Service}/>
-      <Route path="/contact" component={Contact}/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/service" component={Service}/>
+      <Route exact path="/contact" component={Contact}/>
+      <Redirect to="/"/>
     </Switch>
    
       <h1>Welcome</h1>
