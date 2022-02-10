9839c0503b3e03d326619213c649c2b8bcde2068
Tenzin <tenthinten@gmail.com>
2022-02-10 17:27:47 +0530

    modify home page

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,22 @@ import React from 'react';
 const Home =() => {
     return (
         <>
-            <h1>Home</h1>
-        </> 
+            <section id="header" className=''>
+                <div className="container-fluid nav_bg">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1">
+                               <h1>Grow your business with <strong>Tenzin</strong></h1>
+                               <h2 className='my-3'>We are the team of talented developer websites</h2>
+                                <div className='mt-3'>
+                                    <a href="" className='btn-get-started'>Get Started</a>
+                                </div>
+                            </div>
+                </div>
+            </div>
+        </div>
+            </section> 
+        </>
     )
 
 }
